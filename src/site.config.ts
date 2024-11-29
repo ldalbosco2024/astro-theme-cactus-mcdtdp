cd27f75ab0f33886b0c25cc50ba38adfd99c1c0a
ldalbosco2024 <100857523+ldalbosco2024@users.noreply.github.com>
2024-11-29 11:49:51 -0300

    changed dracula to catppuccin-macchiato

--- src/site.config.ts
+++ src/site.config.ts
@@ -71,6 +71,6 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 		return `[data-theme="${theme.name}"]`;
 	},
 	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
-	themes: ["dracula"],
+	themes: ["catppuccin-macchiato"],
 	useThemedScrollbars: false,
 };
