c8110edc891a36f7d3a189687ba65d00b2f04f87
ldalbosco2024 <100857523+ldalbosco2024@users.noreply.github.com>
2024-11-29 12:07:09 -0300

    , to ;

--- src/site.config.ts
+++ src/site.config.ts
@@ -71,6 +71,6 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 		return `[data-theme="${theme.name}"]`;
 	},
 	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
-	themes: ["catppuccin-latte"], ["catppuccin-macchiato"],
+	themes: ["catppuccin-latte"]; ["catppuccin-macchiato"];
 	useThemedScrollbars: true,
 };
